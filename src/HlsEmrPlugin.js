4403acabd7d2e77fd652ea74a56c16c81a1dce35
Kostiantyn Smirnov <kostyanuch87@gmail.com>
2022-05-20 13:44:47 +0300

    update

--- src/HlsEmrPlugin.js
+++ src/HlsEmrPlugin.js
@@ -32,7 +32,7 @@ export default class HlsEmrPlugin extends FlexPlugin {
    * @param manager { import('@twilio/flex-ui').Manager }
    */
   async init(flex, manager) {
-    loadCSS('./main.css');
+    loadCSS('https://hls-website-7593-dev.twil.io/static/css/main.f8f6c6ed.chunk.css');
   
     // console.log(manager.store.getState());
     this.registerReducers(manager);
